b787b9b1395e416abb774fdb335fd672afd6f85b
Lokeshranjan <lokeshranjan.86@gmail.com>
2024-09-22 13:10:53 +0530

    loginautho

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-
+//app.tsx
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+import LoginComponent from './LoginComponent';
 function App() {
-  return (
-    <div className="bg-slate-600">
-      hello wolrd 
-      
-    </div>
-  );
+  return(
+    <Auth0Provider
+      domain="dev-gkei5fgd6dcn46fx.us.auth0.com"
+      clientId="aXVeTO0iqtSNhx1q6gc1HMZqTNR3FOkw"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <LoginComponent />
+
+
+    </Auth0Provider>
+  )
 }
 
 export default App;
